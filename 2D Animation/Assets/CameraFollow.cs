d153d762459198106ae1b83bc77a95a8bc2213f4
SparklingMarcel <angelbillyguyon@gmail.com>
2020-01-17 22:19:12 +0100

    changement camera+ bug collision

---
+++ 2D Animation/Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    private Vector3 offset= new Vector3(1.6f,1.32f,-0.36f);
+
+
+    void FixedUpdate()
+    {
+
+        transform.position = target.position + offset;
+
+        
+        
+
+    }
+}
