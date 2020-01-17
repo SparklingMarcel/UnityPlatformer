7da5d47b106d920804dbc8cdc13ebb074acdb0eb
SparklingMarcel <angelbillyguyon@gmail.com>
2020-01-17 20:21:03 +0100

    Initial commit

---
+++ 2D Animation/Assets/moveScene2d.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class moveScene2d : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+    void onTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("test1");
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("test");
+            SceneManager.LoadScene(newLevel);
+        }
+    }
+
+}
